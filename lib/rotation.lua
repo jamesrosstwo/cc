04c116bec7aed63d8dc67b0adaa568bcdbb2df15
James Ross <james.rosstwo@gmail.com>
2023-12-21 00:12:39 -0800

    Update files

--- lib/rotation.lua
+++ lib/rotation.lua
@@ -1,30 +1,20 @@
 local rotation = {}
 local log4cc = require("lib.log4cc")
+local movement = require("movement")
 
 
-    --[[orientation will be:
-    -x = 1
-    - z = 2
-    + x = 3
-    + z = 4
-    ]]--
+--[[orientation will be:
+-x = 1
+- z = 2
++ x = 3
++ z = 4
+]]--
 function rotation.GetAbsoluteOrientation()
     log4cc.info("Getting absolute rotation")
     local loc1 = vector.new(gps.locate(2, false))
-    if not turtle.forward() then
-        for j = 1, 6 do
-            if not turtle.forward() then
-                turtle.dig()
-            else
-                break
-            end
-        end
-    else
-        turtle.back()
-    end
+    movement.DigMove()
     local loc2 = vector.new(gps.locate(2, false))
     local heading = loc2 - loc1
-    
     return math.floor((heading.x + math.abs(heading.x) * 2) + (heading.z + math.abs(heading.z) * 3))
 end
 
@@ -43,8 +33,6 @@ function rotation.TurnLeft()
     turtle.turnLeft()
     rotation.orientation = (rotation.orientation + 2) % 4 + 1
 end
-
-
 
 function rotation.RotateTowards(desiredOrientation)
     log4cc.info("Rotating towards " .. desiredOrientation)
